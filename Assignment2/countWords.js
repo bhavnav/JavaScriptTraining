bc56e31e60f7eeb7b7378f6f6fd50781fd25fe6f
bhavnav <bhavna_vadlamani@epam.com>
2017-11-10 15:07:21 +0530

    Update countWords.js

--- Assignment2/countWords.js
+++ Assignment2/countWords.js
@@ -1,14 +1,12 @@
-function countWords(words)
-		{
-			words = words.replace(/\s +/ g, ' ');
-			var arr = words.split(" ");
-			var result = new Array();
-			for(i = 0; i < arr.length; i++)
-			{
-				if(arr[i] != "" && arr[i] != " ")
-				{
-					result.push(arr[i]);
-				}
-			}
-			return result.length;
-		}
+function countWords(words) {
+    words = words.replace(/\s +/
+        g, ' ');
+    var arr = words.split(" ");
+    var result = new Array();
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] != "" && arr[i] != " ") {
+            result.push(arr[i]);
+        }
+    }
+    return result.length;
+}
