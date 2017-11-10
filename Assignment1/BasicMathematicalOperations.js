c5ef40809008d4002288ee8e84249840ee65be3c
bhavnav <bhavna_vadlamani@epam.com>
2017-11-10 15:07:57 +0530

    Update BasicMathematicalOperations.js

--- Assignment1/BasicMathematicalOperations.js
+++ Assignment1/BasicMathematicalOperations.js
@@ -1,22 +1,21 @@
-function basicOp(operation, value1, value2){
-  var result;
-  switch(operation)
-  {
-     case '+':
-     	result = value1 + value2;
-     	break;
-     case '-':
-     	result = value1 - value2;
-     	break;
-     case '*':
-     	result = value1 * value2;
-     	break;
-     case '/':
-     	result = value1 / value2;
-     	break;
-     default:
-     	return "Please check your input";
-  }
-  return result;
+function basicOp(operation, value1, value2) {
+    var result;
+    switch (operation) {
+        case '+':
+            result = value1 + value2;
+            break;
+        case '-':
+            result = value1 - value2;
+            break;
+        case '*':
+            result = value1 * value2;
+            break;
+        case '/':
+            result = value1 / value2;
+            break;
+        default:
+            return "Please check your input";
+    }
+    return result;
 }
 		
